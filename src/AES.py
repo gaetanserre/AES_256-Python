2b8b3e36c5dda93aabb9e3240239384e5e71ab91
Gaëtan <gaetan.serre@u-psud.fr>
2020-07-18 16:09:31 +0200

    Strong improvements. Removing PBKDF2 and argon2

--- src/AES.py
+++ src/AES.py
@@ -1,15 +1,16 @@
+import os
 import argparse
 import getpass
-import os
-from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from Crypto.Protocol.KDF import PBKDF2
-import argon2
-
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import scrypt
+from Crypto.Hash import SHA3_512
 
 RED = "\033[1;31m"
 GREEN = "\033[0;32m"
 RESET = "\033[0;0m"
+
+buffer_size = 65536 # 64Kb
 
 def print_red(str):
     print(RED+str+RESET)
@@ -17,23 +18,42 @@ def print_red(str):
 def print_green(str):
     print(GREEN+str+RESET)
 
-buffer_size = 65536 # 64Kb
-is_file = False
-is_dir = False
 
-def encrypt(pwd, input_file_path):
-    salt = get_random_bytes(32)
-    key = PBKDF2(pwd, salt, dkLen=32)
-    hashed_pwd = argon2.low_level.hash_secret(pwd.encode('utf-8'), salt, 16, 33555, 2, 128, argon2.low_level.Type.ID)
-    
+
+def generate_salt():
+    return get_random_bytes(32)
+
+
+def get_salt_from_file(input_file_path):
+    input_file = open(input_file_path, 'rb')
+    return input_file.read(32)
+
+
+def generate_AES256_key(passwd, salt):
+    return scrypt(passwd, salt, 32, N=2**20, r=8, p=1)
+
+
+def checkPwd(passwd, salt, input_file_path):
+    input_file = open(input_file_path, 'rb')
+    bytes_temp = input_file.read(112)
+    hashed_pwd = bytes_temp[48:112]
+
+    return SHA3_512.new(data=passwd.encode('utf-8')).update(salt).digest() == hashed_pwd
+
+
+def encrypt(key, passwd, salt, input_file_path):
+    hashed_passwd = SHA3_512.new(data=passwd.encode('utf-8'))
+    hashed_passwd.update(salt)
+    hashed_passwd = hashed_passwd.digest()
+
     input_file = open(input_file_path, 'rb')
     output_file = open(input_file_path + '.aes', 'wb')
 
     cipher_encrypt = AES.new(key, AES.MODE_CFB)
 
-    output_file.write(salt)
-    output_file.write(cipher_encrypt.iv)
-    output_file.write(cipher_encrypt.iv)
+    output_file.write(salt) #32 bytes
+    output_file.write(cipher_encrypt.iv) #16 bytes
+    output_file.write(hashed_passwd) #64 bytes
 
     buffer = input_file.read(buffer_size)
     while len(buffer) > 0:
@@ -46,15 +66,13 @@ def encrypt(pwd, input_file_path):
     os.remove(input_file_path)
 
 
-def decrypt(pwd, input_file_path):
+def decrypt(key, input_file_path):
     input_file = open(input_file_path, 'rb')
 
-    bytes_temp = input_file.read(295)
-    salt = bytes_temp[:32]
+    bytes_temp = input_file.read(112)
     iv = bytes_temp[32:48]
         
     output_file = open(input_file_path[:-4], 'wb')
-    key = PBKDF2(pwd, salt, dkLen=32)
 
     cipher_decrypt = AES.new(key, AES.MODE_CFB, iv=iv)
 
@@ -69,18 +87,6 @@ def decrypt(pwd, input_file_path):
     os.remove(input_file_path)
 
 
-def checkPwd(pwd, path):
-    input_file = open(path, 'rb')
-    bytes_temp = input_file.read(295)
-    hashed_pwd = bytes_temp[48:295]
-
-    try: 
-        argon2.low_level.verify_secret(hashed_pwd, pwd.encode('utf-8'), argon2.low_level.Type.ID)
-        return True
-    except:
-        return False
-
-    
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -89,10 +95,8 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--decrypt", action = "store_true", default=False)
     args = parser.parse_args()
 
-    if os.path.isfile(args.file):
-        is_file = True
-    if os.path.isdir(args.file):
-        is_dir = True
+    is_file = os.path.isfile(args.file)
+    is_dir = os.path.isdir(args.file)
 
     if args.encrypt and (is_file or is_dir):
         while True:
@@ -104,31 +108,50 @@ if __name__ == "__main__":
                 print_red("Password doesn't match. Please try again.")
 
         if is_file:
+            print("Generating key from password..")
+            salt = generate_salt()
+            key = generate_AES256_key(pwd, salt)
+
             print("Encrypting file...")
-            encrypt(pwd, args.file)
-            print_green("File is ecrypted.")
+            encrypt(key, pwd, salt, args.file)
+
+            print_green("File is encrypted.")
             
         elif is_dir:
             files = []
             for (dirpath, dirnames, filenames) in os.walk(args.file):
                 for f in filenames:
+                    print("Generating key from password..")
+
+                    salt = generate_salt()
+                    key = generate_AES256_key(pwd, salt)
+
                     filename = os.path.join(dirpath, f)
+
                     print("Encrypting " + filename)
-                    encrypt(pwd, filename)
+                    encrypt(key, pwd, salt, filename)
+
                     print_green("File is encrypted.")
             
 
 
     elif args.decrypt and (is_file or is_dir):
         if is_file:
+            salt = get_salt_from_file(args.file)
             while True:
                 pwd = getpass.getpass("Password? : ")
-                if checkPwd(pwd, args.file):
+
+                if checkPwd(pwd, salt, args.file):
+                    print("Generating key from password..")
+                    key = generate_AES256_key(pwd, salt)
+
                     print("Decrypting file...")
-                    decrypt(pwd, args.file)
+                    decrypt(key, args.file)
+
                     break
                 else:
                     print_red("Wrong password. Try again.")
+
             print_green("File is decrypted")
 
         elif is_dir:
@@ -137,9 +160,15 @@ if __name__ == "__main__":
             for (dirpath, dirnames, filenames) in os.walk(args.file):
                 for f in filenames:
                     filename = os.path.join(dirpath, f)
-                    print("Decrypting " + filename)
-                    if checkPwd(pwd, filename):
-                        decrypt(pwd, filename)
+                    salt = get_salt_from_file(filename)
+
+                    print("Generating key from password..")
+
+                    if checkPwd(pwd, salt, filename):
+                        print("Decrypting " + filename)
+                        key = generate_AES256_key(pwd, salt)
+                        decrypt(key, filename)
+
                         print_green("File is decrypted.")
                     else:
                         print_red("Wrong password. Ignoring file.")
@@ -150,3 +179,4 @@ if __name__ == "__main__":
 
     else:
         print_red("The path is wrong. Please try again.")
+
